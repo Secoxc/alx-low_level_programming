d70a0ff1c2249c4ca591029f87e0596b8173132c
Secoxc <sayed@example.com>
2023-07-18 09:07:38 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: prints _putchar and a new line
- *
+ * main - prints _putchar and a new line
  * Return: Always 0
  */
 int main(void)
-{
+
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-}
+
 return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#ifndef MAIN-H
-#define MAIN-H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
+
 int _putchar(char c);
+
 
 #endif
